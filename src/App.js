7f04c035877f8aff1be1d9781f5b4249bee95278
juan-lien-do <findmejuan@hotmail.com>
2024-02-24 17:20:08 -0300

    implement hashrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
-
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import {  useState } from 'react'
-import Landing from './landing/Landing'
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useState } from 'react';
+import Landing from './landing/Landing';
 import Footer from './footer/Footer';
 import Menu from './navbar/Navbar';
 import UCRepartir from './usecase/UCRepartir';
 
 function App() {
   return (
-    <div className='bg-light' style={{position:"relative", paddingBottom:"16em"}}>
-      <div style={{minHeight:"100vh"}}>
-        <Router >
-          <Menu/>
+    <div className='bg-light' style={{ position: "relative", paddingBottom: "16em" }}>
+      <div style={{ minHeight: "100vh" }}>
+        <Router>
+          <Menu />
           <Routes>
             <Route path="/" element={<Landing />} exact />
             <Route path='/repartir' element={<UCRepartir />} exact></Route>
@@ -20,7 +19,7 @@ function App() {
           </Routes>
         </Router>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
