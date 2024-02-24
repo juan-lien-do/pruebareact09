092790676d3c5950f8f2e6f65a675e74b092bc6a
juan-lien-do <findmejuan@hotmail.com>
2024-02-24 00:01:51 -0300

    fix routing

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ import './SCSS/custom.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
