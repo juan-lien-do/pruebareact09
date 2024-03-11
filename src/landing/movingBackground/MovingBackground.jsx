8ec8c080733e46c188a2fa432983a99139916944
juan-lien-do <findmejuan@hotmail.com>
2024-03-11 20:53:30 -0300

    added moving background for landing page

---
+++ src/landing/movingBackground/MovingBackground.jsx
@@ -0,0 +1,15 @@
+import "./movingBackground.css";
+
+export default function MovingBackground() {
+    return (
+        <div id="galeria">
+            <div className="mosaico card">$645978</div>
+            <div className="mosaico card">$12756</div>
+            <div className="mosaico card">$20</div>
+            <div className="mosaico card">$3158</div>
+            <div className="mosaico card">$2356756</div>
+            <div className="mosaico card">$354</div>
+            <div className="mosaico card">$31597</div>
+        </div>
+    );
+}
