ac1da771c498c498c4793783fee0bce4cf37003f
juan-lien-do <findmejuan@hotmail.com>
2024-03-11 20:50:08 -0300

    modified filename

--- src/usecase/emitirTransacciones/downloadPDFButton.jsx
+++ src/usecase/emitirTransacciones/downloadPDFButton.jsx
@@ -6,7 +6,7 @@ const DownloadPDFButton = () => {
     const element = document.documentElement;
     const options = {
       margin: 0.5,
-      filename: 'downloaded.pdf',
+      filename: 'pagosGenerados.pdf',
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
