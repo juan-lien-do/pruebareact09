a5ab5b9793ea90bc93a8f0fd82a2250fe94db272
juan-lien-do <findmejuan@hotmail.com>
2024-04-03 00:24:48 -0300

    agregar comandos de deploy

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import 'bootstrap/dist/css/bootstrap.min.css'
 //import '@popperjs/core/dist/cjs/popper.js'
-//import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './SCSS/custom.css'
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
@@ -22,4 +23,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
