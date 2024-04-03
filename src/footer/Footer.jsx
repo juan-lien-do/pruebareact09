b6bc1948ff05a6b6468401b88b0a74c180067b74
juan-lien-do <findmejuan@hotmail.com>
2024-04-03 00:19:43 -0300

    cambiar datos seccion footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,16 +1,25 @@
-export default function Footer () {
-    return(
-        <footer className="bg-dark" style={{position:"absolute", bottom:0, width:"100%"}}>
+import * as React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+
+
+export default function Footer() {
+    return (
+        <footer className="bg-dark" style={{ position: "absolute", bottom: 0, width: "100%" }}>
             <hr></hr>
             <div className="container py-4 py-md-5 px-4 px-md-3 ">
-                    <div className="col-6 mb-3 text-primary  mx-auto">
-                        <h5>Informacion</h5>
-                        <ul className="list-unstyled">
-                            <li className="mb-2"><a href="https://www.linkedin.com/in/juanestebanliendo/" target="_blank">Contacto</a></li>
-                            <li className="mb-2"><a href="#">Donar</a></li>
-                            <li className="mb-2"><a href="#">Preguntas frecuentes{" (FAQ)"}</a></li>
-                        </ul>
-                    </div>
+                <div className="col-6 mb-3 text-primary  mx-auto">
+                    <h5>Informacion</h5>
+                    <ul className="list-unstyled">
+                        <li className="mb-2"><a href="https://www.linkedin.com/in/juanestebanliendo/" target="_blank">Contacto</a></li>
+                        <li className="mb-2"><a href="https://github.com/juan-lien-do" target="_blank">Otros proyectos</a></li>
+                        <li className="mb-2">
+                            <NavLink to="/faq/" >
+                            Preguntas frecuentes
+                            </NavLink>
+                            </li>
+                    </ul>
+                </div>
             </div>
         </footer>
     );
