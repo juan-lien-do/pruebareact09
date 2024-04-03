a2ed708baed7fcc984756c337fd0bfcc7a6c83d6
juan-lien-do <findmejuan@hotmail.com>
2024-04-03 00:10:45 -0300

    agregar logo al navbar, arreglar zindex

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,18 +1,21 @@
 import { NavLink } from "react-router-dom";
+import imagen from '../landing/pizza.svg';
+import './Navbar.css';
 //import '../SCSS/custom.css'
 //import '../App.css'
 export default function Menu() {
     return (
-        <nav className="shadow navbar navbar-expand-xxxl bg-dark text-white py-0 " style={{position:"sticky",top:"0",zIndex:3}}>
+        <nav className="shadow navbar navbar-expand-xxxl bg-dark text-white py-0 " style={{position:"sticky",top:"0",zIndex:4}}>
             <span className="text-primary ms-4 fw-bold fs-3">
                 <NavLink className="nav-link " to="/repartir/">
+                    <img className="img-fluid shadow-logo" src={imagen} alt="imagen de ejemplo" style={{width:"50px"}} />
                     Repartir
                 </NavLink>
             </span>
             <div className="d-flex me-4 my-2">
                 <ul className="navbar-nav">
                     <li className="navbar-item btn btn-primary py-0 fw-bold fs-5">
-                        <NavLink className="nav-link " to="/repartir/">
+                        <NavLink className="nav-link text-black" to="/faq/">
                             ¿Cómo se usa?
                         </NavLink>
                     </li>
